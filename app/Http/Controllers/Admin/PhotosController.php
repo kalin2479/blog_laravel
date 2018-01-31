409c5fc23c885807958394516e5a1fc47a5c36a4
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2018-01-31 11:42:21 -0500

    avance

--- app/Http/Controllers/Admin/PhotosController.php
+++ app/Http/Controllers/Admin/PhotosController.php
@@ -26,7 +26,9 @@ class PhotosController extends Controller
         // pero podemos indicarle que lo guarde en la carpeta public de ahi dentro
         // de la siguiente manera ('public') o con otro nombre
         // eso nos devolvera la direccion url de la imagen.
-        $photo = $photo->store('public');
+        // vamos a guardarlo en una subcarpeta llamada post como primer parametro
+        // como segundo parametro el disco como tenemos definido en la configuracion
+        $photo = $photo->store('posts','public');
         // para poder acceder a las imagenes que se guardar en la carpeta store/app/public
         // demos crear un simbolic link, para ello artisan nos provee el siguiente comoando.
         // php artisan storage:link
@@ -34,9 +36,19 @@ class PhotosController extends Controller
         $photoUrl = Storage::url($photo);
 
         // almacenamos la imagen
-        Photo::create([
-            'url' => $photoUrl,
-            'post_id' => $post->id
+        // Photo::create([
+        //     'url' => $photoUrl,
+        //     'post_id' => $post->id
+        // ]);
+
+        // Otra forma de crear una foto sin necesidad de pasarle el id del post es de la siguiente manera
+
+        // Utilizamos la variable post para acceder a la relacion photos y creamos una nueva foto,
+        // para crear esta nueva foto solo necesitamos la url ya que el post_id est implicito al guardarlo de
+        // esta forma
+
+        $post->photos()->create([
+            'url' => $photoUrl
         ]);
 
         // return $photoUrl;
@@ -45,15 +57,11 @@ class PhotosController extends Controller
     public function destroy(Photo $photo)
     {
         // eliminamos el registro de la base de datos
+
+        // Ahora como parte de la mejora lo que vamos hacer es que cuando se elimine una foto  desde la base de datos con elocuent
+        // inmediatamente se elimine la foto a la que hace referencia. Para ello utilizaremos el modelo foto.
         $photo->delete();
-
-        // reemplazamos storage por public para poder borrar la imagen del servidor
-        $photoPath = str_replace('storage','public',$photo->url);
-
-        // Para elimniar un archivo utilizamos storage delete
-        // que recibe por parametro la ubicacion del archivos
-        Storage::delete($photoPath);
-
+        
         return back()->with('flash','Foto eliminada');
     }
 }
